2846401965af7d5c2fd281566dff74cc5675d837
Sadid <76834719+Sadid-USN@users.noreply.github.com>
2021-01-20 23:34:37 +0300

    Create convert miles.py
    convert miles to km

---
+++ convert miles.py
@@ -0,0 +1,12 @@
+print("Enter a mile ")
+mile1 = (int(input()))
+kilometer1 = 1.609
+length1 = mile1
+
+def convert_mails(kilometr, length):
+    count = kilometr*length
+    if count >=1.6:
+        return "  miles equals "  + str(count) + " kilometers "
+
+mile = convert_mails(kilometer1,length1)
+print(mile)
